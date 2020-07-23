b93d295eccaf61b674661c7d6047b5c581126a97
Predrag Ristic <pedjasmek@gmail.com>
2020-07-23 10:31:10 +0200

    Additional Eureka config for removing non reachable clients Hystrix support added with dashboard Multiple instances of a client can be registered with Eureka

--- spring-cloud/spring-cloud-eureka/spring-cloud-eureka-feign-client/src/main/java/com/baeldung/spring/cloud/feign/client/FeignClientApplication.java
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-feign-client/src/main/java/com/baeldung/spring/cloud/feign/client/FeignClientApplication.java
@@ -3,25 +3,47 @@ package com.baeldung.spring.cloud.feign.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.netflix.discovery.EurekaClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @Controller
 public class FeignClientApplication {
-    @Autowired
-    private GreetingClient greetingClient;
+	@Autowired
+	private GreetingClient greetingClient;
 
-    public static void main(String[] args) {
-        SpringApplication.run(FeignClientApplication.class, args);
-    }
+	@Autowired
+	private EurekaClient eurekaClient;
 
-    @RequestMapping("/get-greeting")
-    public String greeting(Model model) {
-        model.addAttribute("greeting", greetingClient.greeting());
-        return "greeting-view";
-    }
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientApplication.class, args);
+	}
+
+	@RequestMapping("/get-greeting")
+	@HystrixCommand(commandKey = "getGreeting", fallbackMethod = "getDefaultGreeting", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000") })
+	public String getGreeting(Model model) {
+		model.addAttribute("greeting", greetingClient.getGreeting());
+		return "greeting-view";
+	}
+
+	public String getDefaultGreeting(Model model) {
+		model.addAttribute("greeting", "DEFAULT GREETING");
+		return "greeting-view";
+	}
 }
--- spring-cloud/spring-cloud-eureka/spring-cloud-eureka-feign-client/src/main/java/com/baeldung/spring/cloud/feign/client/GreetingClient.java
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-feign-client/src/main/java/com/baeldung/spring/cloud/feign/client/GreetingClient.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient("spring-cloud-eureka-client")
 public interface GreetingClient {
     @RequestMapping("/greeting")
-    String greeting();
+    String getGreeting();
 }
