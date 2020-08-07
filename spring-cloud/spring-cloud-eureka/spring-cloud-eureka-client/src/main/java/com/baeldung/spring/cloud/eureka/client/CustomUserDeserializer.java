c44591e8e7367332152538e91e3555bb598fb038
Predrag Ristic <predrag.ristic@crealogix.com>
2020-08-07 09:22:56 +0200

    Small refactor of UserDetails deserialization

--- spring-cloud/spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/CustomUserDeserializer.java
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/CustomUserDeserializer.java
@@ -10,26 +10,24 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-
 public class CustomUserDeserializer extends JsonDeserializer<CustomUserDetails> {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public CustomUserDetails deserialize(JsonParser jp, DeserializationContext ctxt)
 			throws IOException, JsonProcessingException {
-		ObjectCodec oc = jp.getCodec();
-	    JsonNode node = oc.readTree(jp);
-	    String username = node.get("username").asText();
-	    String password = node.get("password").asText();
 		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(jp);
+		String username = node.get("username").asText();
+		String password = node.get("password").asText();
 		List<Map<String, String>> authorities = mapper.convertValue(node.get("authorities"), ArrayList.class);
-		List<SimpleGrantedAuthority> grantedAuthorities = authorities.stream().map(authority -> new SimpleGrantedAuthority(authority.get("authority"))).collect(Collectors.toList());
+		List<SimpleGrantedAuthority> grantedAuthorities = authorities.stream()
+				.map(authority -> new SimpleGrantedAuthority(authority.get("authority"))).collect(Collectors.toList());
 		CustomUserDetails user = new CustomUserDetails(username, password, grantedAuthorities);
 		return user;
 	}
--- spring-cloud/spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/CustomUserDetailsService.java
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/CustomUserDetailsService.java
@@ -1,12 +1,6 @@
 package com.baeldung.spring.cloud.eureka.client;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,12 +14,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		Collection<GrantedAuthority> grantedAuthoritiesList = new ArrayList<>();
-//		grantedAuthoritiesList.add(new SimpleGrantedAuthority("ROLE_CUSTOM_USER"));
-//		User user = new User("tutorialspoint", "$2a$08$fL7u5xcvsZl78su29x1ti.dxI.9rYO8t0q5wk2ROJ.1cdR53bmaVG", grantedAuthoritiesList);
-//		return user;
-		CustomUserDetails user =  (CustomUserDetails) userDetailsClient.getClientDetails(username);
-		return user;
+		return (CustomUserDetails) userDetailsClient.getClientDetails(username);
 	}
 
 }
